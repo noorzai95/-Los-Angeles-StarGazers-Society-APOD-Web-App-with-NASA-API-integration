b6d3e0c2a0185933a75fdef5360fa09f349bd11b
noorzai95 <114100835+noorzai95@users.noreply.github.com>
2024-01-25 13:10:33 -0500

    Add files via upload

---
+++ api-template/js/main.js
@@ -0,0 +1,24 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value.toLowerCase()
+  const url = `https://api.nasa.gov/planetary/apod?api_key=cL7bB7XOpvFbzl5tEaGj8CsU0bnxS19k9yoQE3hy&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if(data.media_type ==='image'){
+          document.querySelector('img').src = data.hdurl
+        }else if(data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+          document.querySelector('img').src = null
+        }
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
